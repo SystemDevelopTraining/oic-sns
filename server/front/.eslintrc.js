59292afcd42a2e522343ad2b1d578dcdda30677f
elipmoc <relipmoc101@gmail.com>
2019-08-14 23:55:15 +0900

    [text]サーバーへ接続するテストを作成

--- server/front/.eslintrc.js
+++ server/front/.eslintrc.js
@@ -1,27 +1,27 @@
 module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
-    'parser': '@typescript-eslint/parser',
+    parser: '@typescript-eslint/parser',
   },
-  rules:{
-    "@typescript-eslint/explicit-function-return-type":"off",
-    "@typescript-eslint/func-call-spacing":"error",
-    'prefer-const':'error',
-    'no-else-return' : 'error',
-    'semi': ['error', 'always'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/func-call-spacing': 'error',
+    'prefer-const': 'error',
+    'no-else-return': 'error',
+    semi: ['error', 'always'],
     'no-extra-semi': 'error',
-    'eqeqeq': 'error',
-    'keyword-spacing': [2, {"before": false, "after": true}],
-    "space-infix-ops": "error",
-    "key-spacing":"error",
-    "no-var": "error"
+    eqeqeq: 'error',
+    'keyword-spacing': [2, { before: false, after: true }],
+    'space-infix-ops': 'error',
+    'key-spacing': 'error',
+    'no-var': 'error',
   },
-  plugins: ["vue","@typescript-eslint"],
+  plugins: ['vue', '@typescript-eslint'],
   extends: [
-    "eslint:recommended",
-    'plugin:@typescript-eslint/recommended', 
-    "plugin:vue/recommended",    
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/recommended',
     'prettier/@typescript-eslint',
-    "prettier"
-  ]
+    'prettier',
+  ],
 };
