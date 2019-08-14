1c319bef48207a8175cc342f6368e32b6d68ba55
Daiki431 <dai.take431@gmail.com>
2019-08-15 00:02:18 +0900

    [refactor]importの仕方を変更した

--- server/front/src/router.ts
+++ server/front/src/router.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import Login from "./views/Login.vue";
 
 Vue.use(Router);
 
@@ -17,12 +19,12 @@ export default new Router({
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+            component: About
         },
         {
             path: "/login",
             name: "login",
-            component: () => import("./views/Login.vue")     
+            component: Login
         }
     ]
 });
