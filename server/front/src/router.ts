fed1d21cce58cd116b83559a4483fe5fe52aa991
Daiki431 <dai.take431@gmail.com>
2019-08-19 16:01:39 +0900

    [design]Index.vueを追加し/でアクセスできるようにした

--- server/front/src/router.ts
+++ server/front/src/router.ts
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Index from "./views/Index.vue";
 import About from "./views/About.vue";
 import Login from "./views/Login.vue";
 
@@ -10,8 +10,8 @@ export default new Router({
     routes: [
         {
             path: "/",
-            name: "home",
-            component: Home
+            name: "index",
+            component: Index
         },
         {
             path: "/about",
