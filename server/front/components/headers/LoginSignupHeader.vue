fe458e41a0136fbb5a5e42bdfac030dacc4ab514
Daiki431 <dai.take431@gmail.com>
2019-08-23 22:00:25 +0900

    [design]LoginSignupとDefault用のheaderをcomponentsに作成した

---
+++ server/front/components/headers/LoginSignupHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar app>
+    <v-toolbar-title class="headline text-uppercase">
+      <router-link
+        to="/"
+        tag="span"
+      >
+        <span>OIC-SNS</span>
+      </router-link>
+    </v-toolbar-title>
+    <v-spacer />
+    <v-btn
+      text
+      href="http://localhost:3000/auth-user/v1"
+    >
+      ログイン・サインアップ
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script lang="ts">
+import { Component, Vue }from "vue-property-decorator";
+@Component({})
+export default class extends Vue {}
+</script>
