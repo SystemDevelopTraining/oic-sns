12b1a7e18d7414be3d7e01a90216de7893d43a3b
elipmoc <relipmoc101@gmail.com>
2019-09-14 21:02:55 +0900

    [chore]nestでtypeormを扱いやすくするために設定した

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthUserController } from './auth-user/controllers/auth-user.controller';
 import { AuthUserModule } from './auth-user/auth-user.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormconfig from '../ormconfig';
+import * as ormconfigProd from '../ormconfig.prod';
 
 @Module({
-  imports: [AuthUserModule],
+  imports: [AuthUserModule, UserModule, getTypeOrmModule()],
   controllers: [AppController, AuthUserController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
+
+function getTypeOrmModule() {
+  // --- TypeORMの設定
+  const connectionOptions =
+    process.env.NODE_ENV === 'production' ? ormconfigProd : ormconfig;
+
+  return TypeOrmModule.forRoot(connectionOptions as any);
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,20 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {  createConnection } from 'typeorm'
-import { User } from './entities/user';
-import * as ormconfig from '../ormconfig';
-import * as ormconfigProd from '../ormconfig.prod';
 import passport = require('passport');
 
 async function bootstrap() {
-  // --- TypeORMの設定
-  const connectionOptions =process.env.NODE_ENV === 'production'?ormconfigProd:ormconfig;
-  const connection = await createConnection(connectionOptions as any);
-  let userRepository = connection.getRepository(User);
-  let user = new User();
-  user.age = 777;
-  user.name = "hoge";
-  userRepository.save(user);
   const app = await NestFactory.create(AppModule);
   app.use(passport.initialize());
   await app.listen(process.env.PORT || 3000);
