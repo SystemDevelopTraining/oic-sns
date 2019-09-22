3f68fc1aeac760ef3c4734efe00be2be39547993
elipmoc <relipmoc101@gmail.com>
2019-09-22 16:32:49 +0900

    [chore]テストが通るように修正

--- server/src/domain/user/user.service.spec.ts
+++ server/src/domain/user/user.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { TypeOrmModule, getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
