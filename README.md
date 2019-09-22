41b9c6c0ed276608d4c9c964ff77de5e3afa23fc
elipmoc <relipmoc101@gmail.com>
2019-09-22 16:47:36 +0900

    [chore]ciの設定

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![CircleCI](https://circleci.com/gh/SystemDevelopTraining/oic-sns.svg?style=svg)](https://circleci.com/gh/SystemDevelopTraining/oic-sns)
+
 ## 初期セットアップ
 
 ```
--- server/front/tests/unit/example.spec.ts
+++ server/front/tests/unit/example.spec.ts
@@ -1,4 +1,4 @@
-import { ApiClient } from '../../src/adapters/ApiClient';
+import { ApiClient }from '../../src/adapters/ApiClient';
 
 process.env.API_SERVER_URL = 'http://server:3000';
 
