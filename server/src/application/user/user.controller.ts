2c4dd91e6b8e98378f8ec081444342e9941be049
mugendex <tanariku6134@gmail.com>
2019-09-25 17:00:03 +0900

    [refacter]不要なimportを索条

--- server/src/application/user/user.controller.ts
+++ server/src/application/user/user.controller.ts
@@ -1,10 +1,7 @@
 import { Controller, Get, Res, Post, Req, Body } from '@nestjs/common';
 import { Response } from 'express';
 import { UserService } from '../../domain/user/user.service';
-import { get } from 'https';
-import bodyParser = require('body-parser');
 import {UserDto} from '../../domain/user/user.dto';
-import request = require('superagent');
 
 @Controller('user')
 export class UserController {
