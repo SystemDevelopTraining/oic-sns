38e180d0d07e825cb7d32f364be6a86af24a41f7
elipmoc <relipmoc101@gmail.com>
2019-09-30 16:04:03 +0900

    [design]初期プロフィール設定ページのレイアウト

--- server/front/src/views/UserInitProfilePage.vue
+++ server/front/src/views/UserInitProfilePage.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>
-    初期プロフィール設定ページ
-  </div>
+  <v-card
+    class="mx-auto d-flex flex-column"
+    max-width="600"
+    min-height="550"
+  >
+    <v-card-title class="mx-auto">
+      初期プロフィール設定ページ
+    </v-card-title>
+    <v-card outlined>
+      <v-img
+        class="mx-auto"
+        width="100px"
+        height="100px"
+        src="user_photo.png"
+      />
+    </v-card>
+    <v-form>
+      <v-text-field label="本名" />
+      <v-select
+        label="性別"
+        :items="['男','女']"
+      />
+    </v-form>
+    <v-btn
+      large
+      width="100"
+      class="mt-auto"
+    >
+      登録
+    </v-btn>
+  </v-card>
 </template>
