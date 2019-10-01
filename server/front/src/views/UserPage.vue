17f2f853a47b88370aca322417ad91e5e63c9192
HerberkYuan <wyjxjl@sina.com>
2019-10-01 17:57:31 +0900

    title not yet

--- server/front/src/views/UserPage.vue
+++ server/front/src/views/UserPage.vue
@@ -1,5 +1,42 @@
 <template>
-  <div>
-    userpage
-  </div>
+  <v-card
+    max-width="450"
+    class="mx-auto"
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title>James Bonds</v-list-item-title>
+        <v-list-item-subtitle>
+          Secondary line text Lorem ipsum dolor sit amet,
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          consectetur adipiscing elit.
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title>James Bonds</v-list-item-title>
+        <v-list-item-subtitle>
+          Secondary line text Lorem ipsum dolor sit amet,
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          consectetur adipiscing elit.
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title>James Bonds</v-list-item-title>
+        <v-list-item-subtitle>
+          Secondary line text Lorem ipsum dolor sit amet,
+        </v-list-item-subtitle>
+        <v-list-item-subtitle>
+          consectetur adipiscing elit.
+        </v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-card>
 </template>
