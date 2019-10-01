277932b946162ae38f043de04876401d503a09a6
Leo <b7396@oic.jp>
2019-10-01 13:57:30 +0900

    test

--- server/front/src/views/TopPage.vue
+++ server/front/src/views/TopPage.vue
@@ -1,8 +1,20 @@
 <template>
   <div>
     <h1>Topページ</h1>
-    <v-btn small>
-      Login/Signin
-    </v-btn>
+    <v-card
+      max-width="344"
+      
+      class="spacing-playground mx-auto my-5 px-5"
+    >
+      <v-card-title>I'm a title</v-card-title>
+      <v-card-text>I'm card text</v-card-text>
+    </v-card>
+    <v-card
+      max-width="344"
+      class="spacing-playground mx-auto my-5 px-5"
+    >
+      <v-card-title>I'm a title</v-card-title>
+      <v-card-text>I'm card text</v-card-text>
+    </v-card>
   </div>
 </template>
