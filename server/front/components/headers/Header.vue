6d7335edbcb623d1c248003aa9a4899214ca5874
elipmoc <relipmoc101@gmail.com>
2019-10-01 14:34:57 +0900

    [design]ツールバーのレイアウト

---
+++ server/front/components/headers/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app-bar
+    app
+    bottom
+  >
+    <v-card
+      class="d-flex justify-space-between"
+      width="100%"
+      tile
+      flat
+    >
+      <div class="pa-2">
+        <v-btn max-width="90">
+          Profile
+        </v-btn>
+      </div>
+      <div class="pa-2">
+        <v-btn max-width="50">
+          LOGO
+        </v-btn>
+      </div>
+      <div class="pa-2">
+        <v-btn max-width="90">
+          タイムライン
+        </v-btn>
+      </div>
+    </v-card>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Component, Vue }from 'vue-property-decorator';
+@Component({})
+export default class extends Vue {}
+</script>
