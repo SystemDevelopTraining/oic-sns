7dc7b9c6c7ccbdc356b8b2b5fdf0eabdd4d1aa69
Leo <b7396@oic.jp>
2019-10-01 17:31:37 +0900

    final for followlistpage

--- server/front/src/views/FollowListPage.vue
+++ server/front/src/views/FollowListPage.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>
-    <h1>followList</h1>
-  </div>
+  <v-card>
+    <v-toolbar>
+      <v-btn
+        class="mr-auto"
+        color="orange darken-2"
+        dark
+      >
+        <v-icon
+          dark
+          left
+        >
+          mdi-arrow-left
+        </v-icon>Back
+      </v-btn>
+      <v-toolbar-title>Follow List</v-toolbar-title>
+    </v-toolbar>
+
+    <v-list>
+      <v-list-item class="d-flex flex-row">
+        <p>ruzen is gay</p>
+        <v-btn
+          class="ml-auto"
+          depressed
+          small
+          color="blue"
+        >
+          Follow
+        </v-btn>
+      </v-list-item>
+    </v-list>
+    
+
+    <v-list>
+      <v-list-item class="d-flex flex-row">
+        <p>ruzen is gay</p>
+        <v-btn
+          class="ml-auto"
+          depressed
+          small
+          color="blue"
+        >
+          Follow
+        </v-btn>
+      </v-list-item>
+    </v-list>
+  </v-card>
 </template>
