672df7a2f4b44630c2230fc5845cdf5ec9d10888
mugendex <tanariku6134@gmail.com>
2019-10-01 15:08:01 +0900

    [chore]jwtstrategyの移動に伴うパスの変更

--- server/src/infrastructure/auth-user/auth-user.module.ts
+++ server/src/infrastructure/auth-user/auth-user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthUserController } from '../../application/auth-user/auth-user.controller';
 import { GoogleStrategy } from './strategies/google.strategy';
 import { AuthUserService } from '../../domain/auth-user/services/auth-user.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtStrategy } from '../strategy/jwt.strategy';
 
 @Module({
   controllers: [AuthUserController],
