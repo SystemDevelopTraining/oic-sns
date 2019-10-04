1f09ca1606bb40472c057bb0f5ce1d31c976d961
Leo <b7396@oic.jp>
2019-10-04 17:06:03 +0900

    トップページにログイン/サインアップボタンを作って、ボタンを押したらlocalhost:3000/auth-user/v1に遷移するようにすた

--- server/front/src/views/TopPage.vue
+++ server/front/src/views/TopPage.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <h1>Topページ</h1>
+    <div>
+      <v-btn
+        large
+        color="primary"
+        href="http://localhost:3000/auth-user/v1"
+      >
+        Login/Signup
+      </v-btn>
+    </div>
+
     <v-card
       max-width="344"
-      
       class="spacing-playground mx-auto my-5 px-5"
     >
       <v-card-title>I'm a title</v-card-title>
