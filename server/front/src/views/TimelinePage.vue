7f2bf14663c548c6a56416de8d354dee1dd13ed9
Daiki431 <dai.take431@gmail.com>
2019-10-04 16:42:20 +0900

    [design]タイムラインのレイアウトを作成

--- server/front/src/views/TimelinePage.vue
+++ server/front/src/views/TimelinePage.vue
@@ -1,5 +1,92 @@
 <template>
-  <div>
-    TIME LINE PAGE
-  </div>
+  <v-container
+    fluid
+    class="center"
+  >
+    <v-row>
+      <v-col>
+        <div>
+          <v-btn
+            block
+            color="white"
+          >
+            投稿
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-card
+        class="mx-auto"
+        width="900"
+        outlined
+      >
+        <v-row>
+          <v-col
+            cols="auto"
+            class="pb-0"
+          >
+            <v-card-title>
+              Furutani Hayate
+            </v-card-title>
+          </v-col>
+          <v-col
+            class="pb-0"
+          >
+            <v-row justify="end">
+              <v-list-item-avatar
+                size="80"
+                color="grey"
+              />
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            Lorem ipsum dolor safa;lfjaiwhfalwgnawbgawgwa
+            it amet, churutanibakaunkokasusineonsectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliq.
+          </v-card-text>
+          <v-card-text>
+            2019-1-1
+          </v-card-text>
+        </v-row>
+      </v-card>
+    </v-row>
+    <v-row>
+      <v-card
+        class="mx-auto"
+        width="900"
+        outlined
+      >
+        <v-row>
+          <v-col
+            cols="auto"
+            class="pb-0"
+          >
+            <v-card-title>
+              Furutani Hayate
+            </v-card-title>
+          </v-col>
+          <v-col
+            class="pb-0"
+          >
+            <v-row justify="end">
+              <v-list-item-avatar
+                size="80"
+                color="grey"
+              />
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-card-text>
+            bore et dolore magna aliq.
+          </v-card-text>
+          <v-card-text>
+            2019-1-1
+          </v-card-text>
+        </v-row>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
