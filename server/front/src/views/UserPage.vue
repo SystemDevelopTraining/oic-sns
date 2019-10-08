d9a0b12c045afe53523fe087d32cc28ed06d7884
en <b7049@oic.jp>
2019-10-08 17:41:00 +0900

    [design] userpageをレイアウト作成中

--- server/front/src/views/UserPage.vue
+++ server/front/src/views/UserPage.vue
@@ -1,42 +1,59 @@
 <template>
-  <v-card
-    max-width="450"
-    class="mx-auto"
-  >
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title>James Bonds</v-list-item-title>
-        <v-list-item-subtitle>
-          Secondary line text Lorem ipsum dolor sit amet,
-        </v-list-item-subtitle>
-        <v-list-item-subtitle>
-          consectetur adipiscing elit.
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title>James Bonds</v-list-item-title>
-        <v-list-item-subtitle>
-          Secondary line text Lorem ipsum dolor sit amet,
-        </v-list-item-subtitle>
-        <v-list-item-subtitle>
-          consectetur adipiscing elit.
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-list-item three-line>
-      <v-list-item-content>
-        <v-list-item-title>James Bonds</v-list-item-title>
-        <v-list-item-subtitle>
-          Secondary line text Lorem ipsum dolor sit amet,
-        </v-list-item-subtitle>
-        <v-list-item-subtitle>
-          consectetur adipiscing elit.
-        </v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-  </v-card>
+  <div class="father">
+    <div
+      class="mt-6"
+      align="center"
+    > 
+      <el-row
+        type="flex"
+        class="row-bg"
+        justify="center"
+      >
+        <el-col>
+          <div class="grid-content">
+            <v-avatar
+              color="indigo"
+              size="88"
+            >
+              <v-icon dark>
+                mdi-account-circle
+              </v-icon>
+            </v-avatar>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+  
+    <v-container
+      fluid
+      class="pa-0"
+    >
+      <v-row align="center">
+        <v-col
+          cols="12"
+          sm="6"
+        >
+          <v-btn
+                
+            color="white"
+            size="50"
+            class="ms-20"
+          >
+            フォロー
+          </v-btn>
+          <v-btn
+            size="50"
+            color="colors.red.darken1"
+            class="ms-20"
+          >
+            フォローリスト
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+  </v-col>
+  </v-row>
+  </v-container>
+  </div>
 </template>
