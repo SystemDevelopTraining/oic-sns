599387a163dab42971b07e3ea477152cf639fbce
en <b7049@oic.jp>
2019-10-11 16:13:58 +0900

    [design]userLayoutdesign作成

--- server/front/src/views/UserPage.vue
+++ server/front/src/views/UserPage.vue
@@ -1,59 +1,62 @@
 <template>
   <div class="father">
-    <div
-      class="mt-6"
-      align="center"
-    > 
-      <el-row
-        type="flex"
-        class="row-bg"
-        justify="center"
-      >
-        <el-col>
-          <div class="grid-content">
-            <v-avatar
-              color="indigo"
-              size="88"
-            >
-              <v-icon dark>
-                mdi-account-circle
-              </v-icon>
-            </v-avatar>
-          </div>
-        </el-col>
-      </el-row>
-    </div>
-  
-    <v-container
-      fluid
-      class="pa-0"
+    <v-card
+      class="mx-auto"
+      outlined
     >
-      <v-row align="center">
-        <v-col
-          cols="12"
-          sm="6"
+      <div
+        class="mt-6"
+        align="center"
+      > 
+        <el-row
+          type="flex"
+          class="row-bg"
+          justify="center"
         >
-          <v-btn
-                
-            color="white"
-            size="50"
-            class="ms-20"
+          <el-col>
+            <div class="grid-content">
+              <v-avatar
+                color="indigo"
+                size="88"
+              >
+                <v-icon dark>
+                  mdi-account-circle
+                </v-icon>
+              </v-avatar>
+            </div>
+          </el-col>
+        </el-row>
+      </div>
+    
+      <v-container
+        fluid
+        class="ma-20"
+      >
+        <v-row align="center">
+          <v-card-actions
+            class="d-flex flex-row mx-12"
+            outlined
           >
-            フォロー
-          </v-btn>
-          <v-btn
-            size="50"
-            color="colors.red.darken1"
-            class="ms-20"
-          >
-            フォローリスト
-          </v-btn>
-        </v-col>
-      </v-row>
-    </v-container>
-  </div>
-  </v-col>
-  </v-row>
-  </v-container>
+            <v-col>
+              <v-btn
+                max-width="120"
+                color="white"
+              >
+                フォロー
+              </v-btn>
+            </v-col>
+      
+            <v-col>
+              <v-btn
+                max-width="120"
+                color="colors.red.darken1"
+              >
+                フォローリスト
+              </v-btn>
+            </v-col>
+          </v-card-actions>
+        </v-row>
+      </v-container>
+    </v-card>
   </div>
 </template>
