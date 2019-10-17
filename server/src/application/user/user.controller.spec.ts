4c27ff9991afeeae1d1e58da26492d54aeef8b6b
elipmoc <relipmoc101@gmail.com>
2019-10-18 06:31:41 +0900

    [refactor]googleProfilesDataの依存を分離

--- server/src/application/user/user.controller.spec.ts
+++ server/src/application/user/user.controller.spec.ts
@@ -4,6 +4,7 @@ import { UserService } from '../../domain/user/user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from '../../domain/entities/user.entity';
 import { Following } from '../../domain/entities/following.entity';
+import { GoogleProfilesData } from '../../infrastructure/temp-data/google.profiles.data';
 
 describe('User Controller', () => {
   let controller: UserController;
@@ -13,6 +14,10 @@ describe('User Controller', () => {
       controllers: [UserController],
       providers: [
         UserService,
+        {
+          provide: 'GoogleProfilesRepository',
+          useClass: GoogleProfilesData,
+        },
         {
           provide: getRepositoryToken(User),
           useValue: {},
