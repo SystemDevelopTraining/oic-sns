eb0d3f0e1c432ca961ba9e3b2c5a9b955f298e41
Daiki431 <dai.take431@gmail.com>
2019-10-18 15:37:34 +0900

    [design]フォロー・フォローリストボタンの位置をセンターに移動

--- server/front/src/views/UserPage.vue
+++ server/front/src/views/UserPage.vue
@@ -1,62 +1,47 @@
 <template>
-  <div class="father">
-    <v-card
-      class="mx-auto"
-      outlined
+  <v-card
+    class="mx-auto"
+    outlined
+  >
+    <div
+      class="mt-6"
+      align="center"
     >
-      <div
-        class="mt-6"
-        align="center"
-      > 
-        <el-row
-          type="flex"
-          class="row-bg"
-          justify="center"
-        >
-          <el-col>
-            <div class="grid-content">
-              <v-avatar
-                color="indigo"
-                size="88"
-              >
-                <v-icon dark>
-                  mdi-account-circle
-                </v-icon>
-              </v-avatar>
-            </div>
-          </el-col>
-        </el-row>
-      </div>
-    
-      <v-container
-        fluid
-        class="ma-20"
+      <el-row
+        type="flex"
+        class="row-bg"
+        justify="center"
       >
-        <v-row align="center">
-          <v-card-actions
-            class="d-flex flex-row mx-12"
-            outlined
-          >
-            <v-col>
-              <v-btn
-                max-width="120"
-                color="white"
-              >
-                フォロー
-              </v-btn>
-            </v-col>
-      
-            <v-col>
-              <v-btn
-                max-width="120"
-                color="colors.red.darken1"
-              >
-                フォローリスト
-              </v-btn>
-            </v-col>
-          </v-card-actions>
-        </v-row>
-      </v-container>
-    </v-card>
-  </div>
+        <el-col>
+          <div class="grid-content">
+            <v-avatar
+              color="indigo"
+              size="88"
+            >
+              <v-icon dark>
+                mdi-account-circle
+              </v-icon>
+            </v-avatar>
+          </div>
+        </el-col>
+      </el-row>
+    </div>
+    <v-container
+      fluid
+      class="center"
+    >
+      <v-row>
+        <v-col align="right">
+          <v-btn max-width="120">
+            フォロー
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn max-width="120">
+            フォローリスト
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
