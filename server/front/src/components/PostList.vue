745cd364221492d3bc44d7cc73acc081f1962ddc
Rz <rujen-shakya@hotmail.com>
2019-10-18 17:02:33 +0900

    [feat]投稿コンポーネントを投稿情報を引数にして描画できるようにしました(imported PostInfos版)

--- server/front/src/components/PostList.vue
+++ server/front/src/components/PostList.vue
@@ -11,6 +11,7 @@
 
 <script lang="ts">
 import Post from '../components/Post.vue';
+import PostInfos from '../domain/post/PostInfos.ts';
 import { Component, Vue }from 'vue-property-decorator';
 @Component({ components: { Post } })
 export default class extends Vue {
