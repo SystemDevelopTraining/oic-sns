6353c2aa3f325f784f5599ac685f4ad9c2110fb9
elipmoc <relipmoc101@gmail.com>
2019-10-22 14:47:07 +0900

    [fix]全体のデザインの修正

--- server/front/src/views/TopPage.vue
+++ server/front/src/views/TopPage.vue
@@ -12,17 +12,11 @@
       </v-btn>
     </div>
 
-    <v-card
-      max-width="344"
-      class="spacing-playground mx-auto my-5 px-5"
-    >
+    <v-card class="spacing-playground mx-auto my-5 px-5">
       <v-card-title>I'm a title</v-card-title>
       <v-card-text>I'm card text</v-card-text>
     </v-card>
-    <v-card
-      max-width="344"
-      class="spacing-playground mx-auto my-5 px-5"
-    >
+    <v-card class="spacing-playground mx-auto my-5 px-5">
       <v-card-title>I'm a title</v-card-title>
       <v-card-text>I'm card text</v-card-text>
     </v-card>
