9ffd6eb17fbe5704f822f1d05b8b85575ddf9a6e
elipmoc <relipmoc101@gmail.com>
2019-10-22 21:17:21 +0900

    [fix]サーバのユーザ検索のレスポンスの型を修正

--- server/front/src/domain/user/UserDto.ts
+++ server/front/src/domain/user/UserDto.ts
@@ -2,10 +2,12 @@ import { Sex }from './Sex';
 import { UserId }from './UserId';
 
 export interface UserDto {
-    id: UserId;
-    name: string;
-    sex: Sex;
-    oicNumber: string;
-    birthday: string;
-    note: string;
-  }
+  id: UserId;
+  name: string;
+  sex: Sex;
+  oicNumber: string;
+  birthday?: string;
+  note: string;
+  //自分自身かどうか
+  isMyself: boolean;
+}
