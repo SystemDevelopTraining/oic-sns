e2d3592c2e3b9eb29065be25b4a1dd623fd68d20
elipmoc <relipmoc101@gmail.com>
2019-10-29 23:02:47 +0900

    [feat]フォローアンフォロー周り完璧に完成

--- server/front/src/components/FollowList/FollowUser.vue
+++ server/front/src/components/FollowList/FollowUser.vue
@@ -10,7 +10,7 @@
       color="blue"
       @click="onFollowClick"
     >
-      Follow
+      {{ followText }}
     </v-btn>
   </v-list-item>
 </template>
@@ -18,23 +18,38 @@
 <script lang="ts">
 import { Component, Vue, Prop }from 'vue-property-decorator';
 import { FollowUserDto }from '../../domain/follow_list/followUser.dto';
+import { AsyncOnce }from '../../utils/AsyncOnce';
+import { CreateFollowApplication }from '../../create/CreateFollowApplication';
 @Component({})
 export default class extends Vue {
-  @Prop({ required: true, type: Object }) private follow!: FollowUserDto;
+  @Prop({ required: true, type: Object }) private followUser!: FollowUserDto;
+
+  asyncOnce = new AsyncOnce();
+
+  followText: 'フォロー' | 'フォロー解除' = this.followUser.isFollow
+    ? 'フォロー解除'
+    : 'フォロー';
 
   get name() {
-    return this.follow.name;
+    return this.followUser.name;
   }
 
   onUserClick() {
     this.$router.push({
       name: 'user',
-      params: { id: String(this.follow.id.id) },
+      params: { id: String(this.followUser.id.id) },
     });
   }
 
   onFollowClick() {
-    this.$emit('click');
+    this.asyncOnce.Do(this.follow);
+  }
+
+  async follow() {
+    const result = await CreateFollowApplication().FollowOrUnfollow(
+      this.followUser.id,
+    );
+    this.followText = result.isFollow ? 'フォロー解除' : 'フォロー';
   }
 }
 </script>
