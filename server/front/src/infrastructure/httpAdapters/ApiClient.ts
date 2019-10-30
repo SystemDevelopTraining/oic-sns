8e0e3587e13218c2ac054c7006031768d9bd0cde
elipmoc <relipmoc101@gmail.com>
2019-10-30 09:08:27 +0900

    [chore]axiosをキャッシュして高速化

--- server/front/src/infrastructure/httpAdapters/ApiClient.ts
+++ server/front/src/infrastructure/httpAdapters/ApiClient.ts
@@ -1,5 +1,3 @@
-import *as Axios from 'axios';
-import { EnvManager }from '../../utils/EnvManager';
 import { CreateUserParams }from './Protocol';
 import { MakeUserResult }from '../../domain/user/MakeUserResult';
 import { UserId }from '../../domain/user/UserId';
@@ -10,19 +8,14 @@ import { CreatePostParamsDto }from '../../domain/post/CreatePostParamsDto';
 import { CreatePostResult }from '../../domain/post/CreatePostResult';
 import { SearchPostParamsDto }from '../../domain/post/SearchPostParamsDto';
 import { FollowResult }from '../../domain/follow/FollowResult';
+import { CreateAxios, MyAxios }from './CreateAxios';
 
 // サーバーにやり取りをするclass
 export class ApiClient {
-  private axios: Axios.AxiosInstance;
+  private axios: MyAxios;
 
-  public constructor(jwt?:string) {
-    this.axios = Axios.default.create({
-      baseURL: EnvManager.ApiServerUrl,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${jwt}`,
-      },
-    });
+  public constructor(jwt?: string) {
+    this.axios = CreateAxios(jwt);
   }
   // サーバーのルートにアクセス
   public async GetRoute(): Promise<string> {
@@ -36,6 +29,7 @@ export class ApiClient {
   // ユーザーIDからユーザー情報取得する
   public async GetUser(id: UserId): Promise<UserDto> {
     return (await this.axios.get('user/v1/' + id.id, {
+      useCache: true
     })).data;
   }
   // 作成したユーザーデータをサーバーとやり取りをする関数
@@ -55,18 +49,19 @@ export class ApiClient {
   // jwtから自身のユーザIDを取得する
   public async GetMyUserId(): Promise<UserId> {
     const response = await this.axios.get('user/v1/my_user', {
+      useCache: true
     });
     return response.data;
   }
 
   public async GetFollowList(id: UserId): Promise<FollowListDto> {
-    return (await this.axios.get('user/v1/' + id.id + '/follows')).data;
+    return (await this.axios.get('user/v1/' + id.id + '/follows',{useCache: true})).data;
   }
   //最新10件の投稿を返す
   public async TakeLatestPosts(
     params: SearchPostParamsDto,
   ): Promise<PostInfos[]> {
-    const response = await this.axios.get('timeline/v1/latest', { params });
+    const response = await this.axios.get('timeline/v1/latest', { params ,useCache: true});
     return response.data;
   }
 
@@ -80,9 +75,9 @@ export class ApiClient {
   }
 
   //jwtが正しいか確認する
-  public async CheckJwt():Promise<boolean>{
+  public async CheckJwt(): Promise<boolean> {
     try {
-      await this.axios.get('auth-user/v1/jwt_check');
+      await this.axios.get('auth-user/v1/jwt_check',{useCache: true});
     }catch (error) {
       return false;
     }
@@ -90,7 +85,7 @@ export class ApiClient {
   }
 
   //フォローアンフォローリクエストする
-  public async FollowOrUnFollow({id}:UserId):Promise<FollowResult>{
+  public async FollowOrUnFollow({ id }: UserId): Promise<FollowResult> {
     return (await this.axios.post("user/v1/follow/" + id)).data;
   }
 }
