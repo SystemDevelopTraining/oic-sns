3f315c7ef75616aadb685976d7bdaaa7429ec4d8
elipmoc <relipmoc101@gmail.com>
2019-11-01 14:52:26 +0900

    [refactor]投稿一覧クラスを生成して処理を分割した

--- server/front/src/components/post/PostList.vue
+++ server/front/src/components/post/PostList.vue
@@ -2,7 +2,7 @@
   <div>
     <new-post-get-button @click="onLatestPostGetClick" />
     <post
-      v-for="(postInfos,index) in postInfosList"
+      v-for="(postInfos,index) in postInfosListDto"
       :key="index"
       :post-infos="postInfos"
     />
@@ -16,17 +16,22 @@
 <script lang="ts">
 import Post from './Post.vue';
 import NewPostGetButton from './NewPostGetButton.vue';
-import { PostInfos }from '../../domain/post/PostInfos';
 import { Component, Vue, Prop, Watch }from 'vue-property-decorator';
 import { CreatePostApplication }from '../../create/CreatePostApplication';
 import { UserId }from '../../domain/user/UserId';
 import { AsyncOnce }from '../../utils/AsyncOnce';
+import { PostInfosList }from '../../domain/post/PostInfosList';
 @Component({ components: { Post, NewPostGetButton } })
 export default class extends Vue {
   @Prop({ type: Object, required: false }) filterUserId: UserId | undefined;
   asyncOnce = new AsyncOnce();
 
-  postInfosList: PostInfos[] = [];
+  postInfosList: PostInfosList | null = null;
+
+  get postInfosListDto() {
+    return this.postInfosList ? this.postInfosList.PostInfosListDto : [];
+  }
+
   created() {
     this.setPostInfosList();
   }
@@ -37,21 +42,14 @@ export default class extends Vue {
   }
 
   async setPostInfosList() {
-    this.postInfosList = await CreatePostApplication().GetLatestPosts({
-      userId: this.filterUserId ? this.filterUserId.id.toString() : undefined,
-    });
+    this.postInfosList = await CreatePostApplication().GetPostInfosList(
+      this.filterUserId,
+    );
   }
 
   //現在の一番古い投稿より古い最新の投稿を取得する
   async oldPostGet() {
-    if (this.postInfosList.length > 0) {
-      const lastOldPost = this.postInfosList[this.postInfosList.length - 1];
-      const newPostInfosList = await CreatePostApplication().GetLatestPosts({
-        userId: this.filterUserId ? this.filterUserId.id.toString() : undefined,
-        basePostId: String(lastOldPost.id.id),
-      });
-      newPostInfosList.forEach(x => this.postInfosList.push(x));
-    }
+    if (this.postInfosList)await this.postInfosList.GetUpdateOldPost();
   }
 
   onLatestPostGetClick() {
@@ -60,15 +58,7 @@ export default class extends Vue {
 
   //現在の一番新しい投稿より最新の投稿を取得する
   async latestPostGet() {
-    if (this.postInfosList.length > 0) {
-      const lastPost = this.postInfosList[0];
-      const newPostInfosList = await CreatePostApplication().GetLatestPosts({
-        userId: this.filterUserId ? this.filterUserId.id.toString() : undefined,
-        basePostId: String(lastPost.id.id),
-        after: true,
-      });
-      newPostInfosList.reverse().forEach(x => this.postInfosList.unshift(x));
-    }
+    if (this.postInfosList)await this.postInfosList.GetUpdateLatestPost();
   }
 }
 </script>
