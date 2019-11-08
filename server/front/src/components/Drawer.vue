77dd1947126b5e82928ff2bec4f2f41f48273e35
Rz <rujen-shakya@hotmail.com>
2019-11-08 16:11:52 +0900

    [design]navigation-drawerを設定しました

---
+++ server/front/src/components/Drawer.vue
@@ -0,0 +1,80 @@
+
+<template>
+  <div>
+    <template v-if="isLogin">
+      <v-btn
+        color="red"
+        dark
+        small
+        fixed
+        top
+        app
+        fab
+        left
+        @click.stop="drawer = !drawer"
+      >
+        <v-icon>mdi-view-list</v-icon>
+      </v-btn>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        app
+        temporary
+      >
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/78.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>John Leider</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider />
+
+        <v-list dense>
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+        <template v-slot:append>
+          <div class="pa-2">
+            <logout-btn />
+          </div>
+        </template>
+      </v-navigation-drawer>
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+import LogoutBtn from '../components/LogoutBtn.vue';
+import { Component, Vue }from 'vue-property-decorator';
+import { CreateLoginApplication }from '../create/CreateLoginApplication';
+
+@Component({ components: { LogoutBtn } })
+export default class extends Vue {
+  drawer = null;
+  items = [
+    { title: 'プロファイル編集', icon: 'question_answer' },
+    { title: '退会', icon: 'dashboard' },
+  ];
+
+  isLogin = CreateLoginApplication().IsLogin();
+}
+</script>
+
+
+
+
