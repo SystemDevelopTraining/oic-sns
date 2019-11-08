8c1ab4c83ad49bcdcd2828a74536099b8e65c2cf
Daiki431 <dai.take431@gmail.com>
2019-11-08 17:46:47 +0900

    [style]if文の廃止とURLの文字数制限

--- server/front/src/domain/validationRules/EditProfilePageRules.ts
+++ server/front/src/domain/validationRules/EditProfilePageRules.ts
@@ -1,35 +1,31 @@
 export const urlRules = [
     (v: string) => {
-        if (v !== ''){
         const pattern = /^(http|https|ftp):\/\/([A-Z0-9][A-Z0-9_-]*(?:\.[A-Z0-9][A-Z0-9_-]*)+):?(\d+)?\/?/i;
-        return pattern.test(v) || '正しいURLを入力してください';
-        }
-    }
+        return v === '' || pattern.test(v) || '正しいURLを入力してください';
+    },
+    (v: string) => v.length <= 255 || '255文字以内で入力してください'
 ];
 
 export const qiitaUrlRules = [
     (v:string)=>{
-        if (v !== ''){
         const pattern = /^https:\/\/qiita\.com\/./;
-        return pattern.test(v) || '正しいURLを入力してください';
-        }
-    }
+        return v === '' || pattern.test(v) || '正しいURLを入力してください';
+    },
+    (v: string) => v.length <= 255 || '255文字以内で入力してください'
 ];
 
 export const twitterUrlRules = [
     (v:string)=>{
-        if (v !== ''){
         const pattern = /^https:\/\/twitter\.com\/./;
-        return pattern.test(v) || '正しいURLを入力してください';
-        }
-    }
+        return v === '' || pattern.test(v) || '正しいURLを入力してください';
+    },
+    (v: string) => v.length <= 255 || '255文字以内で入力してください'
 ];
 
 export const gitHubUrlRules = [
     (v:string)=>{
-        if (v !== ''){
         const pattern = /^https:\/\/github\.com\/./;
-        return pattern.test(v) || '正しいURLを入力してください';
-        }
-    }
+        return v === '' || pattern.test(v) || '正しいURLを入力してください';
+    },
+    (v: string) => v.length <= 255 || '255文字以内で入力してください'
 ];
