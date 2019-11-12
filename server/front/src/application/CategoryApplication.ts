20c81960fa7af05928c2ab48aea4e598924af546
らいパン粉 <relipmoc101@gmail.com>
2019-11-12 17:14:45 +0900

    Update CategoryApplication.ts

--- server/front/src/application/CategoryApplication.ts
+++ server/front/src/application/CategoryApplication.ts
@@ -6,7 +6,7 @@ export class CategoryApplication {
     public constructor(repository: CategoryRepository){
         this.repository = repository;
     }
-//分野種別一覧取得する
+    //分野種別一覧取得する
     public async GetCategoryItems(): Promise<CategoryDto[]> {
         return this.repository.GetCategoryItems();
     }
