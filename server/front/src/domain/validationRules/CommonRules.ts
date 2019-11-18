79c888e0d51bc17fa6974e5d8162e08ce0c3f26a
Daiki431 <dai.take431@gmail.com>
2019-11-18 13:39:06 +0900

    [feat]投稿の文字数を255文字に制限をかけカウンターを表示した

--- server/front/src/domain/validationRules/CommonRules.ts
+++ server/front/src/domain/validationRules/CommonRules.ts
@@ -17,3 +17,12 @@ export const classNumberRules = [
       return pattern.test(v) || '正しいクラス番号を入力してください';
     },
   ];
+
+export const  postRules = [
+  (v: string) => !!v || '投稿内容を入力してください。',
+  (v: string) => v.length <= 255 || '255文字以内で入力してください',
+  (v: string) => {
+    const pattern = /\S\s*?/;
+    return pattern.test(v) || '空白、改行だけの投稿はできません。';
+  },
+];
