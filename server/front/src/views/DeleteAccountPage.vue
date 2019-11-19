ff676e492af70807b2047a006dd7abe4ee98671b
En Siko <b7049@oic.jp>
2019-11-19 18:28:56 +0900

    [feat]OicNumber確認のやつ作成中

--- server/front/src/views/DeleteAccountPage.vue
+++ server/front/src/views/DeleteAccountPage.vue
@@ -1,52 +1,78 @@
 <template>
-  <v-card>
-    <v-toolbar
-      dark
-      dense
-    >
-      <back-btn />
+  <v-form v-model="haha">
+    <v-card>
+      <v-toolbar
+        dark
+        dense
+      >
+        <back-btn />
 
-      <v-toolbar-title>アカウント削除</v-toolbar-title>
-    </v-toolbar>
-    <v-card height>
-      <v-card-title class="justify-center">
-        <p class="font-weight-black">
-          OICity アカウント削除の確認
-        </p>
-      </v-card-title>
-      <v-card-text class="justify-center">
-        アカウントを削除すると元に戻すことができません。投稿もすべて削除されます。確認の為、学籍番号を入力してください。
-      </v-card-text>
-      <v-container class="grey lighten-5">
-        <v-row
-          align="start"
-          justify="center"
-        >
-          <v-col align-self="end">
-            <v-text-field
-              label="学籍番号"
-              outlined
-              clearable
-            />
-            <v-btn
-              tile
-              color="red"
-              dark
-            >
-              アカウントを削除
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-container>
+        <v-toolbar-title>アカウント削除</v-toolbar-title>
+      </v-toolbar>
+      <v-card height>
+        <v-card-title class="justify-center">
+          <p class="font-weight-black">
+            OICity アカウント削除の確認
+          </p>
+        </v-card-title>
+        <v-card-text class="justify-center">
+          アカウントを削除すると元に戻すことができません。投稿もすべて削除されます。確認の為、学籍番号を入力してください。
+        </v-card-text>
+        <v-container class="grey lighten-5">
+          <v-row
+            align="start"
+            justify="center"
+          >
+            <v-col align-self="end">
+              <v-text-field
+                label="学籍番号"
+                outlined
+                clearable
+                :placeholder="oicNumber"
+                :rules="oicNumberRules"
+              />
+              <v-btn
+                tile
+                color="red"
+                dark
+                :disabled="!haha"
+              >
+                アカウントを削除
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
     </v-card>
-  </v-card>
+  </v-form>
 </template>
 
 <script lang="ts">
 import { Component, Vue }from 'vue-property-decorator';
 import BackBtn from '../components/BackBtn.vue';
+import { CreateUserApplication }from '../create/CreateUserApplication';
 @Component({
   components: { BackBtn },
 })
-export default class extends Vue {}
+export default class extends Vue {
+  oicNumber: string = '';
+  //get OICNumberのためにhahaとoicN作った
+  haha = true;
+  oicN: string[] = [];
+  async created() {
+    const myUser = await CreateUserApplication().GetMyUser();
+    this.oicNumber = myUser.oicNumber;
+    this.oicN = myUser.oicNumber.split('b');
+  }
+  //get OICNumber
+  get oicNumberRules() {
+    alert(this.oicN[1]); //??????alert でサーバに渡したのはなぜundifinedか
+    return [
+      (x: string) =>
+        x === this.oicNumber ||
+        'B' + this.oicN[1] ||
+        '正しく学籍番号を入力してください！',
+    ];
+  }
+}
 </script>
