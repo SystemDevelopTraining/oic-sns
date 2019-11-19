7da9a50c0966f8208a22f3338e80d3570b5c9126
Rz <rujen-shakya@hotmail.com>
2019-11-19 17:39:33 +0900

    [design]background-imageを確定しました。

--- server/front/src/views/TopPage.vue
+++ server/front/src/views/TopPage.vue
@@ -1,39 +1,22 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col
-        xl="9"
-        lg="7"
-        md="7"
-        sm="7"
-        cols="6"
-      >
-        <h1>OICity</h1>
-      </v-col>
-      <v-col
-        xl="3"
-        lg="5"
-        md="5"
-        sm="5"
-        cols="6"
-        align-self="center"
-        justify-self="end"
-      >
-        <v-img
-          v-if="isNotLogin()"
-          src="google_btn.png"
-          @click="onLogin"
-        />
-      </v-col>
-    </v-row>
-    <v-card class="spacing-playground mx-auto my-5 px-5">
-      <v-card-title>どんなSNSなのか？</v-card-title>
-      <v-card-text>OIC内のコミュニティの輪の発展を促進するSNS！</v-card-text>
-    </v-card>
-    <v-card class="spacing-playground mx-auto my-5 px-5">
-      <v-card-title>モチベーション</v-card-title>
-      <v-card-text>OICを愛する気持ちで出来ています！</v-card-text>
-    </v-card>
+    <p
+      style="font-family : 'Lucida HandWriting' ;font-size:50px; color: red"
+      class="text-center font-weight-bold"
+    >
+      Welcome
+      <br>To <br>OiCity
+    </p>
+    <v-row />
+    <!--<v-row><v-img src="oic.jpg" /></v-row>-->
+    <div>
+      <v-img
+        v-if="isNotLogin()"
+        src="google_btn.png"
+        align="end"
+        @click="onLogin"
+      />
+    </div>
   </v-container>
 </template>
 
