a69665ed495f11b3f2ce8abd132a32e18fc8a74d
elipmoc <relipmoc101@gmail.com>
2019-11-19 14:13:26 +0900

    [chore]vue-scrolltoのインストール

--- server/front/src/components/Scroller.vue
+++ server/front/src/components/Scroller.vue
@@ -1,31 +1,26 @@
 <template>
   <div>
-    <template v-if="isLogin">
-      <v-btn
-        color="primary"
-        dark
-        small
-        fixed
-        top
-        app
-        fab
-        right
-        @click.stop="drawer = !drawer"
-      >
-        <v-icon>mdi-chevron-triple-up</v-icon>
-      </v-btn>
-    </template>
+    <v-btn
+      color="primary"
+      dark
+      small
+      fixed
+      top
+      app
+      fab
+      right
+      @click.stop="drawer = !drawer"
+    >
+      <v-icon>mdi-chevron-triple-up</v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue }from 'vue-property-decorator';
-import { CreateLoginApplication }from '../create/CreateLoginApplication';
 
 @Component({ components: {} })
-export default class extends Vue {
-  isLogin = CreateLoginApplication().IsLogin();
-}
+export default class extends Vue {}
 </script>
 
 
