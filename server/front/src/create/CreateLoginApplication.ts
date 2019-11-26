4916264a16ee6e4898d4adc9f771114b47b232bf
elipmoc <relipmoc101@gmail.com>
2019-11-26 20:57:09 +0900

    [refactor]loginデータ扱いの分岐処理を簡潔に

--- server/front/src/create/CreateLoginApplication.ts
+++ server/front/src/create/CreateLoginApplication.ts
@@ -1,7 +1,12 @@
 import { LoginApplication }from '../application/LoginApplication';
 import {  LoginRepositoryImpl }from '../infrastructure/login/LoginRepositoryImpl';
 import { LoginServiceImpl }from '../infrastructure/login/LoginServiceImpl';
+import { PwaChecker }from '../utils/PwaChecker';
+import { LoginRepositoryForPwaImpl }from '../infrastructure/login/LoginRepositoryForPwaImpl';
 
 export function CreateLoginApplication(): LoginApplication {
-  return new LoginApplication(new LoginRepositoryImpl(),new LoginServiceImpl());
+  return new LoginApplication(
+    PwaChecker.isPwa ? new LoginRepositoryForPwaImpl() : new LoginRepositoryImpl(),
+    new LoginServiceImpl()
+    );
 }
