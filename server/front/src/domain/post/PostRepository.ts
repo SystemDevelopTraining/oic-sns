9b26284f74ccaa37abfa63f47f893354f3e6b283
elipmoc <relipmoc101@gmail.com>
2019-12-03 17:35:55 +0900

    [style]lint fuck

--- server/front/src/domain/post/PostRepository.ts
+++ server/front/src/domain/post/PostRepository.ts
@@ -1,9 +1,9 @@
-import { CreatePostParamsDto } from './CreatePostParamsDto';
-import { CreatePostResult } from './CreatePostResult';
-import { PostInfos } from './PostInfos';
-import { SearchPostParamsDto } from './SearchPostParamsDto';
-import { PostId } from './PostId';
-import { CommentInfosDto } from '../comment/CommentInfosDto';
+import { CreatePostParamsDto }from './CreatePostParamsDto';
+import { CreatePostResult }from './CreatePostResult';
+import { PostInfos }from './PostInfos';
+import { SearchPostParamsDto }from './SearchPostParamsDto';
+import { PostId }from './PostId';
+import { CommentInfosDto }from '../comment/CommentInfosDto';
 
 export interface PostRepository {
   Save(createPostParams: CreatePostParamsDto): Promise<CreatePostResult>;
