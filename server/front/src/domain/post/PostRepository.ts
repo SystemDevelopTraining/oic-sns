e1ef20c3299586aaac63cf4d8d27ef74d805b5e7
elipmoc <relipmoc101@gmail.com>
2019-12-03 17:26:24 +0900

    [feat]フロントで投稿のコメント一覧取得リクエストをできるように

--- server/front/src/domain/post/PostRepository.ts
+++ server/front/src/domain/post/PostRepository.ts
@@ -1,11 +1,13 @@
-import { CreatePostParamsDto }from './CreatePostParamsDto';
-import { CreatePostResult }from './CreatePostResult';
-import { PostInfos }from './PostInfos';
-import { SearchPostParamsDto }from './SearchPostParamsDto';
-import { PostId }from './PostId';
+import { CreatePostParamsDto } from './CreatePostParamsDto';
+import { CreatePostResult } from './CreatePostResult';
+import { PostInfos } from './PostInfos';
+import { SearchPostParamsDto } from './SearchPostParamsDto';
+import { PostId } from './PostId';
+import { CommentInfosDto } from '../comment/CommentInfosDto';
 
 export interface PostRepository {
   Save(createPostParams: CreatePostParamsDto): Promise<CreatePostResult>;
-  TakeLatest(SearchPostParams:SearchPostParamsDto): Promise<PostInfos[]>;
-  Delete(id:PostId):Promise<unknown>;
+  TakeLatest(SearchPostParams: SearchPostParamsDto): Promise<PostInfos[]>;
+  Delete(id: PostId): Promise<unknown>;
+  GetPostComment(id: PostId): Promise<CommentInfosDto[]>;
 }
--- server/front/src/infrastructure/post/PostRepositoryImpl.ts
+++ server/front/src/infrastructure/post/PostRepositoryImpl.ts
@@ -7,6 +7,7 @@ import { CreateLoginApplication }from '../../create/CreateLoginApplication';
 import { SearchPostParamsDto }from '../../domain/post/SearchPostParamsDto';
 import { UnauthorizedErrorHook }from '../UnauthorizedErrorHook';
 import { PostId }from '../../domain/post/PostId';
+import { CommentInfosDto }from '../../domain/comment/CommentInfosDto';
 
 export class PostRepositoryImpl implements PostRepository {
   public Save(createPostParams: CreatePostParamsDto): Promise<CreatePostResult> {
@@ -14,13 +15,18 @@ export class PostRepositoryImpl implements PostRepository {
     return new ApiClient(jwt).CreatePost(createPostParams).catch(UnauthorizedErrorHook);
   }
 
-  public Delete(id:PostId):Promise<unknown>{
+  public Delete(id: PostId): Promise<unknown> {
     const jwt = CreateLoginApplication().GetJwt();
     return new ApiClient(jwt).DeletePost(id).catch(UnauthorizedErrorHook);
   }
 
-  public TakeLatest(searchPostParams:SearchPostParamsDto): Promise<PostInfos[]> {
+  public TakeLatest(searchPostParams: SearchPostParamsDto): Promise<PostInfos[]> {
     const jwt = CreateLoginApplication().GetJwt();
     return new ApiClient(jwt).TakeLatestPosts(searchPostParams).catch(UnauthorizedErrorHook);
+  }
+
+  public GetPostComment(postId: PostId): Promise<CommentInfosDto[]> {
+    const jwt = CreateLoginApplication().GetJwt();
+    return new ApiClient(jwt).GetPostComments(postId);
   }
 }
