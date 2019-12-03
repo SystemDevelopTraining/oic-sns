33befd2c1ab9ba939fb294bc89093450351c49c4
Rz <b7134@oic.jp>
2019-12-03 15:32:57 +0900

    [feat]commentRules add

---
+++ server/front/src/domain/validationRules/PostAndCommentFormRules.ts
@@ -0,0 +1,17 @@
+export const  postRules = [
+    (v: string) => !!v || '投稿内容を入力してください。',
+    (v: string) => v.length <= 255 || '255文字以内で入力してください',
+    (v: string) => {
+      const pattern = /\S\s*?/;
+      return pattern.test(v) || '空白、改行だけの投稿はできません。';
+    },
+  ];
+
+  export const  commentRules = [
+    (v: string) => !!v || 'コメント内容を入力してください。',
+    (v: string) => v.length <= 255 || '255文字以内で入力してください',
+    (v: string) => {
+      const pattern = /\S\s*?/;
+      return pattern.test(v) || '空白、改行だけのコメントの投稿はできません。';
+    },
+  ];
--- server/front/src/domain/validationRules/PostFormRules.ts
+++
@@ -1,8 +0,0 @@
-export const  postRules = [
-    (v: string) => !!v || '投稿内容を入力してください。',
-    (v: string) => v.length <= 255 || '255文字以内で入力してください',
-    (v: string) => {
-      const pattern = /\S\s*?/;
-      return pattern.test(v) || '空白、改行だけの投稿はできません。';
-    },
-  ];
