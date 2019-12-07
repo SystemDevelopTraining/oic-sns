0c44f982005c0685edcc82e6bcfe288ec8ca86a6
elipmoc <relipmoc101@gmail.com>
2019-12-08 03:15:35 +0900

    [style]lint format

--- server/front/src/domain/comment/CommentRepository.ts
+++ server/front/src/domain/comment/CommentRepository.ts
@@ -1,7 +1,7 @@
-import { CreateCommentParamsDto } from './CreateCommentParamsDto';
-import { CreateCommentResult } from './CreateCommentResult';
-import { PostId } from '../post/PostId';
-import { CommentInfosDto } from '../comment/CommentInfosDto';
+import { CreateCommentParamsDto }from './CreateCommentParamsDto';
+import { CreateCommentResult }from './CreateCommentResult';
+import { PostId }from '../post/PostId';
+import { CommentInfosDto }from '../comment/CommentInfosDto';
 
 export interface CommentRepository {
     Save(createCommentParams: CreateCommentParamsDto, postId: PostId): Promise<CreateCommentResult>;
