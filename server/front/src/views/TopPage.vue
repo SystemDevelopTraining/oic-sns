6f663b2efbb872fffd5d98c26e629e8e36a864d4
Daiki431 <dai.take431@gmail.com>
2019-12-20 15:49:55 +0900

    [design]画像の表示位置を変更し、それに伴う画像のトリミング

--- server/front/src/views/TopPage.vue
+++ server/front/src/views/TopPage.vue
@@ -11,7 +11,6 @@
                 max-width="200"
               />
             </v-row>
-
             <v-card-title class="headline">
               What am I?
             </v-card-title>
@@ -45,25 +44,13 @@
           :key="i"
           cols="12"
         >
-          <v-card :color="item.color">
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title
-                  class="headline"
-                  v-text="item.title"
-                />
-
-                <v-card-subtitle v-text="item.artist" />
-              </div>
-
-              <v-avatar
-                class="ma-3"
-                size="125"
-                tile
-              >
-                <v-img :src="item.src" />
-              </v-avatar>
-            </div>
+          <v-card color="white">
+            <v-img :src="item.src" />
+            <v-card-title
+              class="headline"
+              v-text="item.title"
+            />
+            <v-card-subtitle v-text="item.artist" />
           </v-card>
         </v-col>
       </v-row>
@@ -79,27 +66,23 @@ export default class extends Vue {
   items = [
     // the cards wont look nice if you remove the images
     {
-      color: 'white',
       src: 'post.png',
       title: 'Post',
       artist:
         'Just begin by posting your own stuffs, like the kadais of the school or even your feelings about today.',
     },
     {
-      color: 'white',
       src: 'timeline.png',
       title: 'Watch',
       artist: 'Watch people posting their owns so that you can find your type.',
     },
     {
-      color: 'white',
       src: 'comment.png',
       title: 'Comment',
       artist:
         'Finally, just comment if you are interested in the post or even the person. Here you get the chance to make a friend or a project partner.',
     },
     {
-      color: 'white',
       src: 'logo.png',
       title: 'Make Friends',
       artist:
